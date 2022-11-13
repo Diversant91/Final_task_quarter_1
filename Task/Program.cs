cda46020ecfaac80bb2d5571adc28167d4dceecb
Diversant91 <lex0422@mail.ru>
2022-11-13 20:37:17 +0700

    код c# - решение задачи

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] array1 = new string[7] {"7589", "33", "final", "task", "111", "quarter_1", "=)"};
+string[] array2 = new string[array1.Length];
+
+void ArrayIf(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+ArrayIf(array1, array2);
+PrintArray(array2);
